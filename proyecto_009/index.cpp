fbd9c8c7b6d621555ecdf426f7ef947aed168ce5
JerickTwO <jerickjjtm774@gmail.com>
2024-11-14 16:57:15 -0500

    mayor de 3 numeros

--- proyecto_009/index.cpp
+++ proyecto_009/index.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
+#include <string>
 using namespace std;
-int main(){
-    int x = 4, y = 5, aux;
-    aux = x;
-    x =y;
-    y = aux;
-
-    cout << "x = " << x << ", y = " << y << endl; // Outputs: x = 5, y = 5
+int main()
+{
+    int n1 = 212121, n2 = 3233, n3 = 14802399;
+    string Resultado = "";
+    if (n1 == n2 && n2 == n3)
+    {
+        Resultado = "Todos son iguales";
+    }
+    else if (n1 > n2 && n1 > n3)
+    {
+        Resultado = "El primer numero es mayor";
+    }
+    else if (n2 > n1 && n2 > n3)
+    {
+        Resultado = "El segundo numero es mayor";
+    }
+    else
+    {
+        Resultado = "El tercer numero es mayor";
+    }
+    cout << "Resultado: " << Resultado << endl;
     return 0;
 }
